7487dbab2e16f993e8891e3c1929e859273fe72d
Matias Ochoa <matiasmochoa@gmail.com>
2024-09-21 20:34:47 +0200

    Including form - Matias

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,26 @@
 export default function Contact() {
   return (
     <>
-      <h1 className="text-2xl text-white">Form</h1>
+      <div className="container mx-auto ">
+        <h2 className="mb-10 text-3xl font-black text-center text-white uppercase">
+          Contact Form
+        </h2>
+
+        <form className="px-5 py-10 mx-56 mb-10 rounded-lg shadow-lg bg-zinc-500">
+          <div className="mb-5">
+            <label htmlFor="name" className="text-sm font-bold uppercase">
+              Name
+            </label>
+
+            <input
+              type="text"
+              id="name"
+              className="w-full p-3 border border-gray-300"
+              placeholder="Name"
+            />
+          </div>
+        </form>
+      </div>
     </>
   );
 }
