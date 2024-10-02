8132f101ea83fdb545cb75b9a23a6cb178202044
Matias <mattiochoax@gmail.com>
2024-10-03 01:40:02 +0200

    Changes to the modal window - Matias

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,95 +1,127 @@
 import {
-  DialogPanel,
-  DialogTitle,
-  TransitionChild,
-  Transition,
-  Dialog,
+	DialogPanel,
+	DialogTitle,
+	TransitionChild,
+	Transition,
+	Dialog,
 } from "@headlessui/react";
 import { Fragment, useEffect, useState } from "react";
 import { useAppStore } from "../stores/useAppStore";
 
 export default function Modal() {
-  const [content, setContent] = useState({ paragraphs: [] });
-  const modal = useAppStore((state) => state.modal);
-  const closeModal = useAppStore((state) => state.closeModal);
+	const [content, setContent] = useState({ languages: [] });
+	const modal = useAppStore((state) => state.modal);
+	const closeModal = useAppStore((state) => state.closeModal);
 
-  useEffect(() => {
-    fetch("/content/content.json")
-      .then((response) => response.json())
-      .then((data) => setContent(data));
-  }, []);
+	useEffect(() => {
+		fetch("/content/content.json")
+			.then((response) => response.json())
+			.then((data) => setContent(data));
+	}, []);
 
-  return (
-    <>
-      <Transition appear show={modal} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <TransitionChild
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-70" />
-          </TransitionChild>
+	return (
+		<>
+			<Transition appear show={modal} as={Fragment}>
+				<Dialog as="div" className="relative z-50" onClose={closeModal}>
+					<TransitionChild
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0">
+						<div className="fixed inset-0 bg-black bg-opacity-70" />
+					</TransitionChild>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex items-center justify-center min-h-full p-4 text-center">
-              <TransitionChild
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <DialogPanel className="relative px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform rounded-lg shadow-xl bg-gradient-to-r from-slate-700 to-slate-900 sm:my-8 sm:w-full sm:max-w-2xl sm:p-6">
-                  <DialogTitle
-                    as="h3"
-                    className="my-5 text-4xl font-extrabold text-center text-white"
-                  >
-                    Matias Ochoa
-                    <img
-                      src="/public/foto_matias.jpg"
-                      alt="My modal picture"
-                      className="mx-auto my-2 rounded-lg w-96"
-                    />
-                  </DialogTitle>
-                  <DialogTitle
-                    as="h3"
-                    className="my-5 text-2xl font-extrabold text-center text-white uppercase"
-                  >
-                    Work
-                  </DialogTitle>
-                  <DialogTitle
-                    as="h3"
-                    className="my-5 text-2xl font-extrabold text-white"
-                  >
-                    {content.paragraphs.map((paragraph, index) => (
-                      <p key={index} className="text-lg leading-8 text-white">
-                        {paragraph}
-                      </p>
-                    ))}
-                  </DialogTitle>
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex items-center justify-center min-h-full p-4 text-center">
+							<TransitionChild
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95">
+								<DialogPanel className="relative px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform rounded-lg shadow-xl bg-gradient-to-r from-slate-700 to-slate-900 sm:my-8 sm:w-full sm:max-w-2xl sm:p-6">
+									<DialogTitle
+										as="h3"
+										className="my-5 text-4xl font-extrabold text-center text-white">
+										More about me...
+									</DialogTitle>
+									<DialogTitle as="div" className="flex gap-2 my-5">
+										<div className="flex flex-col w-full gap-2 ">
+											<div className="flex flex-col w-full p-4 rounded-lg opacity-75 bg-slate-900">
+												<h3 className="text-xl font-bold text-center text-orange-400">
+													Languages
+												</h3>
 
-                  <div className="flex justify-between gap-4 mt-5">
-                    <button
-                      type="button"
-                      className="inline-flex items-center justify-center w-full h-16 px-10 py-0 text-xl font-semibold text-center text-gray-200 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid rounded-full cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline"
-                      onClick={closeModal}
-                    >
-                      Cerrar
-                    </button>
-                  </div>
-                </DialogPanel>
-              </TransitionChild>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-    </>
-  );
+												{content.languages.map((languages) => (
+													<p className="text-lg text-white">{languages}</p>
+												))}
+											</div>
+
+											<div className="flex flex-col w-full p-4 rounded-lg opacity-75 bg-slate-900">
+												<h3 className="text-xl font-bold text-center text-red-700">
+													Programming Skills
+												</h3>
+												<p className="text-lg text-white">HTML</p>
+												<p className="text-lg text-white">CSS</p>
+												<p className="text-lg text-white">Javascript</p>
+												<p className="text-lg text-white">Sass</p>
+												<p className="text-lg text-white">Tailwind CSS</p>
+												<p className="text-lg text-white">React</p>{" "}
+												{/* Incluir un dropdown para mostrar las skills por lenguaje */}
+												<p className="text-lg text-white">NodeJS</p>
+												<p className="text-lg text-white">Express</p>
+												<p className="text-lg text-white">MongoDB</p>
+												<p className="text-lg text-white">MySQL</p>
+												<p className="text-lg text-white">Python</p>
+												<p className="text-lg text-white">Git</p>
+												<p className="text-lg text-white">Docker</p>
+											</div>
+										</div>
+
+										<div className="flex flex-col w-full gap-2 ">
+											<div className="flex flex-col w-full p-4 rounded-lg opacity-75 bg-slate-900">
+												<h3 className="text-xl font-bold text-center text-teal-400">
+													Soft Skills
+												</h3>
+												<p className="text-lg text-white">Quick Learner</p>
+												<p className="text-lg text-white">Team Player</p>
+												<p className="text-lg text-white">Problem Solver</p>
+												<p className="text-lg text-white">Proactivity</p>
+												<p className="text-lg text-white">
+													Analytical Thinking
+												</p>
+												<p className="text-lg text-white">Communication</p>
+												<p className="text-lg text-white">Adaptability</p>
+												<p className="text-lg text-white">Creativity</p>
+												<p className="text-lg text-white">
+													Attention to Detail
+												</p>
+												<p className="text-lg text-white">
+													Incident Management
+												</p>
+											</div>
+										</div>
+									</DialogTitle>
+
+									<div className="flex justify-between gap-4 mt-5">
+										<button
+											type="button"
+											className="inline-flex items-center justify-center w-full h-16 px-10 py-0 text-xl font-semibold text-center text-gray-200 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid rounded-full cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline"
+											onClick={closeModal}>
+											Close
+										</button>
+									</div>
+								</DialogPanel>
+							</TransitionChild>
+						</div>
+					</div>
+				</Dialog>
+			</Transition>
+		</>
+	);
 }
--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,68 +1,88 @@
 import { IconBriefcase } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 export default function WorkExperience() {
-  return (
-    <>
-      <div className="container mx-auto">
-        <section className="flex items-center justify-center gap-4 mt-10 border-b-2 border-white sm:mt-52 border-opacity-70 border-">
-          <IconBriefcase stroke={2} color="white" size={50} />
-          <h2 className="text-4xl text-white sm:text-6xl">Work Experience</h2>
-        </section>
+	const [content, setContent] = useState({
+		currentExperience: [],
+	});
 
-        <section className="flex flex-col gap-2 mt-10 sm:gap-4 sm:pl-4 sm:border-l-2 sm:border-red-700 sm:border-opacity-80">
-          <h2 className="p-2 text-3xl text-center text-white sm:text-start sm:p-0">
-            <span>Microsoft</span>: Sr. Technical Support Engineer
-          </h2>
-          <span className="text-lg font-bold text-center text-red-500 sm:text-start">
-            2024 - Now
-          </span>
+	useEffect(() => {
+		fetch("/content/aboutMe.json")
+			.then((response) => response.json())
+			.then((data) => setContent(data));
+	});
 
-          <div>
-            <p className="w-full p-4 font-bold text-center text-white sm:p-0 sm:w-1/2 sm:text-start">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates tempora quo adipisci hic corporis delectus repellendus
-              debitis inventore! Sequi, assumenda! Minus delectus natus illo
-              cumque voluptas! Ipsum cum molestias beatae!
-            </p>
-          </div>
-        </section>
+	return (
+		<>
+			<div className="container p-6 mx-auto">
+				<section className="flex items-center justify-center gap-4 mt-10 border-b-2 border-white sm:mt-52 border-opacity-70 border-">
+					<IconBriefcase stroke={2} color="white" size={50} />
+					<h2 className="text-4xl text-white sm:text-6xl">Work Experience</h2>
+				</section>
 
-        <section className="flex flex-col gap-2 mt-10 sm:gap-4 sm:pr-4 sm:border-r-2 sm:border-red-700 sm:items-end sm:border-opacity-80">
-          <h2 className="p-2 text-3xl text-center text-white sm:text-end sm:p-0">
-            <span>Microsoft</span>: Technical Support Engineer
-          </h2>
-          <span className="text-lg font-bold text-center text-red-500 sm:text-end">
-            2022 - 2024
-          </span>
+				<section className="flex flex-col gap-2 mt-10 sm:gap-4 sm:pl-4 sm:border-l-2 sm:border-red-700 sm:border-opacity-80">
+					<h2 className="p-2 text-3xl text-center text-white sm:text-start sm:p-0">
+						<span>Microsoft</span>: Sr. Technical Support Engineer
+					</h2>
+					<span className="text-lg font-bold text-center text-red-500 sm:text-start">
+						2024 - Now
+					</span>
 
-          <div className="flex flex-col sm:items-end">
-            <p className="p-4 font-bold text-center text-white w-ful sm:p-0 sm:w-1/2 sm:text-end">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates tempora quo adipisci hic corporis delectus repellendus
-              debitis inventore! Sequi, assumenda! Minus delectus natus illo
-              cumque voluptas! Ipsum cum molestias beatae!
-            </p>
-          </div>
-        </section>
+					<div>
+						<p className="w-full p-4 font-bold text-center text-white sm:p-0 sm:w-1/2 sm:text-start">
+							{content.currentExperience.map((experience, index) => (
+								<p
+									key={index}
+									className={
+										index === 0
+											? "text-yellow-500 font-bold"
+											: index === 1
+											? "text-hite font-bold"
+											: "text-white font-bold"
+									}>
+									{experience}
+								</p>
+							))}
+						</p>
+					</div>
+				</section>
 
-        <section className="flex flex-col gap-2 mt-10 sm:gap-4 sm:pl-4 sm:border-l-2 sm:border-red-700 sm:border-opacity-80">
-          <h2 className="p-2 text-3xl text-center text-white sm:p-0 sm:text-start">
-            <span>Xandr</span>: Sr Analyst - Product Support
-          </h2>
-          <span className="text-lg font-bold text-center text-red-500 sm:text-start">
-            2021 - 2022
-          </span>
+				<section className="flex flex-col gap-2 mt-10 sm:gap-4 sm:pr-4 sm:border-r-2 sm:border-red-700 sm:items-end sm:border-opacity-80">
+					<h2 className="p-2 text-3xl text-center text-white sm:text-end sm:p-0">
+						<span>Microsoft</span>: Technical Support Engineer
+					</h2>
+					<span className="text-lg font-bold text-center text-red-500 sm:text-end">
+						2022 - 2024
+					</span>
 
-          <div>
-            <p className="w-full p-4 font-bold text-center text-white sm:text-start sm:p-0 sm:w-1/2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates tempora quo adipisci hic corporis delectus repellendus
-              debitis inventore! Sequi, assumenda! Minus delectus natus illo
-              cumque voluptas! Ipsum cum molestias beatae!
-            </p>
-          </div>
-        </section>
-      </div>
-    </>
-  );
+					<div className="flex flex-col sm:items-end">
+						<p className="p-4 font-bold text-center text-white w-ful sm:p-0 sm:w-1/2 sm:text-end">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Voluptates tempora quo adipisci hic corporis delectus repellendus
+							debitis inventore! Sequi, assumenda! Minus delectus natus illo
+							cumque voluptas! Ipsum cum molestias beatae!
+						</p>
+					</div>
+				</section>
+
+				<section className="flex flex-col gap-2 mt-10 sm:gap-4 sm:pl-4 sm:border-l-2 sm:border-red-700 sm:border-opacity-80">
+					<h2 className="p-2 text-3xl text-center text-white sm:p-0 sm:text-start">
+						<span>Xandr</span>: Sr Analyst - Product Support
+					</h2>
+					<span className="text-lg font-bold text-center text-red-500 sm:text-start">
+						2021 - 2022
+					</span>
+
+					<div>
+						<p className="w-full p-4 font-bold text-center text-white sm:text-start sm:p-0 sm:w-1/2">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Voluptates tempora quo adipisci hic corporis delectus repellendus
+							debitis inventore! Sequi, assumenda! Minus delectus natus illo
+							cumque voluptas! Ipsum cum molestias beatae!
+						</p>
+					</div>
+				</section>
+			</div>
+		</>
+	);
 }
