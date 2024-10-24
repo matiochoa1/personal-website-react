75710b6d645c64613055f17fc01b980ed9015037
Matias <mattiochoax@gmail.com>
2024-10-24 23:10:37 +0200

    Adding updated cv

--- src/components/DownloadCv.tsx
+++ src/components/DownloadCv.tsx
@@ -6,7 +6,7 @@ export default function DownloadCv() {
 			<div className="container mx-6 lg:mx-60">
 				<div className="mt-4">
 					<a
-						href="/files/matias-cv.pdf"
+						href="/files/matias-ochoa-cv.pdf"
 						download
 						className="inline-flex items-center px-4 py-2 text-white bg-teal-400 rounded-md hover:bg-teal-500">
 						<IconDownload className="mr-2" />
